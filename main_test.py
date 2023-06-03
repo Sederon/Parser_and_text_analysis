1688492002710bd380e7d88bc5b8eb4ec5d715fd
Sederon <134531995+Sederon@users.noreply.github.com>
2023-06-03 14:34:35 +0300

    User interface developed. Error in list_view model appeared.

--- main_test.py
+++ main_test.py
@@ -8,7 +8,7 @@ def read_text(file_path):
 
 # # Example text
 # text = read_text("Text\\bbc_news1.txt")
-# sum_method = 1  # 0 for cosine method, 1 - LexRank method, 2 - LSA, 3 - Luhn
+sum_method = 1  # 0 for cosine method, 1 - LexRank method, 2 - LSA, 3 - Luhn
 # text_proc = TextProcessing(text, sum_method)
 # text_proc.print_result()
 
@@ -16,4 +16,17 @@ parse = ParseToolAPI()
 data = parse.return_result()
 # print(data[0][['title', 'flair']])
 # print(data[0][data[0]['flair'] == 'Political'][['title', 'flair']])
+# print(data[2].groupby('flair')['title'].count())
+
+print(data[0][data[0]['flair'] == 'Political'][['title', 'flair']])
+
+# for index, row in data[0].iterrows():
+#     try:
+#         content = row['content']
+#         result = TextProcessing(content, sum_method)
+#         result.print_result()
+#     except:
+#         print("{0} Not read!".format(index))
+
+
 
